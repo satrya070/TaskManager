bd92f54a783701aa1b49abe01dc2082e0cf6f575
satrya070 <satrya@spotr.ai>
2025-02-28 18:37:05 +0100

    select basic tasks

--- src/Task.cpp
+++ src/Task.cpp
@@ -1,1 +1,4 @@
 #include "Task.h"
+
+
+Task::Task(std::string _task_name) : task_name(_task_name) {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "hello there" << std::endl;
+    int choice;
+    //std::cin >> choice;
+    choice = 1;
+
+    switch (choice) {
+        case 0:
+            std::cout << "show tasks"; break;
+        case 1:
+            std::cout << "add task"; break;
+        case 2:
+            std::cout << "delete task"; break;
+    } 
+
     return 0;
 }
