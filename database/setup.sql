238d7f0c22d3e5102803deda0c47f0f935c29b6e
satrya070 <satrya@spotr.ai>
2025-03-01 14:00:48 +0100

    add basic tasks database sql setup script

---
+++ database/setup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS tasks (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name varchar(255) NOT NULL,
+	deadline DATE,
+	done BOOLEAN DEFAULT false
+);
+
+-- some example tasks
+INSERT INTO tasks (name, deadline)
+VALUES
+	('Create a database A', '2025-01-02'),
+	('Write script B', '2025-01-03'),
+	('Create tests C', '2025-02-02');
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "sqlite3.h"
 
 int main() {
+    sqlite3* db;
+    sqlite3_open("MyDB", &db);
+
+    std::string sql = "";
+    if (sqlite3_exec(db, sql.c_str(), nullptr, nullptr, nullptr) == SQLITE_OK) {
+        std::cout << "Task added succesfully!" << std::endl;
+    }
+
     int choice;
     //std::cin >> choice;
     choice = 1;
